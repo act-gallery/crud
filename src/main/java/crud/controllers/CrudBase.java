976cef1e4cafc02fa7329c218848012f59369d31
Gelin Luo <greenlaw110@gmail.com>
2018-12-13 22:09:10 +1100

    fix CrudBase type lookup error

--- src/main/java/crud/controllers/CrudBase.java
+++ src/main/java/crud/controllers/CrudBase.java
@@ -10,6 +10,7 @@ import org.osgl.util.Generics;
 
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Map;
 
 public abstract class CrudBase<ID, T, D extends DaoBase<ID, T, ?>> {
     @NoBind
@@ -24,13 +25,14 @@ public abstract class CrudBase<ID, T, D extends DaoBase<ID, T, ?>> {
     }
 
     private void exploreTypes() {
+        Map<String, Class> typeParamLookup = Generics.buildTypeParamImplLookup(getClass());
         List<Type> types = Generics.typeParamImplementations(getClass(), CrudBase.class);
         int sz = types.size();
         if (sz < 3) {
             throw new IllegalArgumentException("Cannot determine DAO type");
         }
         Type daoType = types.get(2);
-        BeanSpec spec = BeanSpec.of(daoType, Act.injector());
+        BeanSpec spec = BeanSpec.of(daoType, Act.injector(), typeParamLookup);
         DaoLoader loader = Act.getInstance(DaoLoader.class);
         dao = $.cast(loader.load(spec));
     }
